c26b1d250f9dd61b73753b22ccada4036f887d1c
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2024-01-18 20:45:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,12 @@ const toBool = (x) => x === "true";
 
 module.exports = {
   LOGS: toBool(process.env.LOGS) || true, //false for if you don't want show the logs
-  HANDLERS:
-    process.env.HANDLER === "false" || process.env.HANDLER === "null"
-      ? "^[.]" // forfor null just "^" add this
-      : "^[.]",
+  HANDLERS:process.env.HANDLER === "false" || process.env.HANDLERS || '^,',
   BRANCH: "master",
   OWNER_NUMBER: ["919961857267@s.whatsapp.net", "0@s.whatsapp.net"],
   STICKER_DATA: process.env.STICKER_DATA || "CLASH-WA-BOT;TOXIC-KICHUX",
   SUDO: process.env.SUDO || "919961857267,0",
-  API: process.env.API || "https://api-viper.onrender.com", //don't change this link. If you change then some commands will not work!.
+  API: process.env.API || "https://api-viper-x.koyeb.app", //don't change this link. If you change then some commands will not work!.
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   OWNER_NAME: process.env.OWNER_NAME || "TOXIC-KICHU",
