069b23aa3c412f124bd1562137e6fcc3f06953fb
ERRORS <102719170+S-U-P-E-R-I-O-R@users.noreply.github.com>
2024-01-18 07:01:32 +0530

    Update menu.js

--- components/menu.js
+++ components/menu.js
@@ -8,12 +8,13 @@ async ({msg}) => {
 const speed = now() - now();
 let [date, time] = new Date().toLocaleString("en-IN", {timeZone: "Asia/Kolkata"}).split(",");
 let menu = `     *[${config.BOT_NAME.toLowerCase()}]*
-________________________
+
+
+
 *➛ 𝘜𝘴𝘦𝘳 :${msg.pushName}*
 *➛ 𝘖𝘸𝘯𝘦𝘳 :${config.OWNER_NAME.toLowerCase()}*
 *➛ 𝘙𝘢𝘮 :${formatp(os.totalmem() - os.freemem())}/${formatp(os.totalmem())}*
 *➛ 𝘔𝘰𝘥𝘦 :${config.WORK_TYPE.toLowerCase()}*\n`
-________________________
 let cmnd = [];
 let cmd;
 let category = [];
